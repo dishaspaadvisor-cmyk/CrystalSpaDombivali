ee10c8af228831826b8a23e5c9903fd853bac18d
Rajan <dos.rajanverma@gmail.com>
2026-06-15 17:16:35 +0530

    sitemap

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: SITE.title || "Orange Spa",
   description: "Orange Spa Nagpur - Relaxation & Wellness",
+  icons:{
+    icon:"/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
