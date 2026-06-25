e76ce058d2fa2e4d530754130d3aaaa2bfce1c87
Rajan <dos.rajanverma@gmail.com>
2026-06-25 17:21:15 +0530

    add another tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FlotingButton from "../components/FlotingButton";
 import { SITE } from "../lib/siteConfig";
+import Script from "next/script";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,8 +19,8 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: SITE.title || "Orange Spa",
   description: "Orange Spa Dombivali - Relaxation & Wellness",
-  icons:{
-    icon:"/favicon.png",
+  icons: {
+    icon: "/favicon.png",
   },
 };
 
@@ -30,9 +31,26 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <meta name="google-site-verification" content="0Tn5Ii84ekGysx4hjpDv4SKP87exOB5BRLYfIOc-Y1Y" />
+        <meta
+          name="google-site-verification"
+          content="0Tn5Ii84ekGysx4hjpDv4SKP87exOB5BRLYfIOc-Y1Y"
+        />
       </head>
+
       <body className="min-h-full flex flex-col">
+         <Script
+    src="https://www.googletagmanager.com/gtag/js?id=AW-18271592496"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-ads" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'AW-18271592496');
+    `}
+  </Script>
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
