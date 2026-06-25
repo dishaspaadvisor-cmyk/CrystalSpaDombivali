04d7385b2cc8b4c90487f1acf54c9cef4fffee75
Rajan <dos.rajanverma@gmail.com>
2026-06-25 17:28:41 +0530

    add tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import React from "react";
+
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import FlotingButton from "../components/FlotingButton";
 import { SITE } from "../lib/siteConfig";
-import Script from "next/script";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -38,23 +40,63 @@ export default function RootLayout({ children }) {
       </head>
 
       <body className="min-h-full flex flex-col">
-         <Script
-    src="https://www.googletagmanager.com/gtag/js?id=AW-18271592496"
-    strategy="afterInteractive"
-  />
 
-  <Script id="google-ads" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-18271592496');
-    `}
-  </Script>
+        {/* Google Ads Global Site Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18271592496"
+          strategy="afterInteractive"
+        />
+
+        {/* Google Ads Initialization */}
+        <Script id="google-ads-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+
+            function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            window.gtag = gtag;
+
+            gtag('js', new Date());
+
+            gtag('config', 'AW-18271592496');
+          `}
+        </Script>
+
+        {/* Conversion Function */}
+        <Script id="google-conversion-function" strategy="afterInteractive">
+          {`
+            window.gtag_report_conversion = function(url) {
+
+              var callback = function () {
+                if (typeof url !== 'undefined') {
+                  window.location = url;
+                }
+              };
+
+              gtag('event', 'conversion', {
+                send_to: 'AW-18271592496/0ggeCOm3tMUcELDAyYhE',
+                value: 1.0,
+                currency: 'INR',
+                transaction_id: '',
+                event_callback: callback
+              });
+
+              return false;
+            };
+          `}
+        </Script>
+
         <Navbar />
-        <main className="flex-1">{children}</main>
+
+        <main className="flex-1">
+          {children}
+        </main>
+
         <Footer />
         <FlotingButton />
+
       </body>
     </html>
   );
